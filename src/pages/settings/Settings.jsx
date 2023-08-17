22ac680fca35035076514f3a8bc8c0c2be100e6d
anandu <sonysandr@gmail.com>
2023-08-18 01:02:35 +0530

    settings2

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -4,7 +4,32 @@ import "./settings.css";
 export default function Settings() {
   return (
     <div className="settings">
-      <div className="settingsWrapper">Settings</div>
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsUpdateTitle">Update your Account</span>
+          <span className="settingsDeleteTitle">Delete Account</span>
+        </div>
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img
+              src="https://images.pexels.com/photos/5063450/pexels-photo-5063450.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+            />
+            <label htmlFor="fileInput">
+              <i className="settingsPPIcon fa-regular fa-user"></i>
+            </label>
+            <input type="file" id="fileInput" style={{display:'none'}} />
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder="Jane" />
+          <label >Email</label>
+          <input type="email" placeholder="Email" />
+          <label >Password</label>
+          <input type="password" placeholder="password" />
+          <button className="settingsSubmit">Update</button>
+        </form>
+      </div>
       <Sidebar />
     </div>
   );
